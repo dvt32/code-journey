3b54b8c846e772ac8269200226a029d807b4d9d2
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-02-29 19:16:48 +0200

    Added files via upload

---
+++ LISP/Uni-Ruse/Lab01/2.2.lisp
@@ -0,0 +1,16 @@
+﻿;;;; 2 задача: Даден е списъка (Alfa Beta Gama Delta Epsilon). Да се напише комбинация от функции, която като резултат да добавя елемента Kapa:
+;;;; 	a. преди първия елемент на дадения списък;
+;;;; 	b. между първия и втория елемент на дадения списък;
+;;;; 	c. като последен елемент на дадения списък.
+
+;; Define list
+(setq myList '(Alfa Beta Gama Delta Epsilon))
+
+;; Solve 'a'
+(cons 'Kapa myList)
+
+;; Solve 'b'
+(cons (car myList) (cons 'Kapa (cdr myList)))
+
+;; Solve 'c'
+(append myList (list 'Kapa))
