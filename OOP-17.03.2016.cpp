90d931ab12515120f2517fa29d873d6df15e176d
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-03-17 12:51:47 +0200

    Create OOP-17.03.2016.cpp

---
+++ OOP-17.03.2016.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include <iostream>
+#include <list>
+#include <vector>
+
+class Figure {
+public:
+	virtual void drawStuff() = 0;
+};
+
+class Circle : public Figure {
+public:
+	void drawStuff() {
+		std::cout << "Drawing circle." << std::endl;
+	}
+};
+
+
+int main()
+{
+	std::vector<Figure*> myVector;
+	// Figure *circle = new Circle();
+	myVector.push_back(new Circle());
+
+	for (std::vector<Figure*>::iterator it = myVector.begin(); it != myVector.end(); ++it) 
+	{
+		(*it)->drawStuff();
+	}
+
+	return 0;
+}
+
