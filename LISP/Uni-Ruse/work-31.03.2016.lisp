8b889b18dbbcc067b44e14c704d01f96ff6115f5
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-03-31 09:35:51 +0300

    Update work-31.03.2016.lisp

--- LISP/Uni-Ruse/work-31.03.2016.lisp
+++ LISP/Uni-Ruse/work-31.03.2016.lisp
@@ -20,3 +20,46 @@
         ((and (numberp (car input)) (< 9 (car input) 100))
          (f2 (cdr input)))
         (t (cons (car input) (f2 (cdr input))))))
+        
+;;;; 3.	Даден е списък на хората, работещи в дадена фирма, който съдържа за всеки човек 
+;;;; - име, 
+;;;; - брой отработени дни за месеца и 
+;;;; - дневна тарифна ставка (сумата, която получават за 1 работен ден). 
+;;;; Да се дефинира функция, която претърсва списъка и 
+;;;; - извежда имената на всички хора, 
+;;;; - които имат дневна тарифна ставка, равна на дадена 
+;;;; - и са отработили не по-малко от определен брой дни за месеца.
+
+;; TODO: Finish f3-print-people
+;; TODO: Format code
+
+;; Make person
+(defun f3-make-person (name work-days daily-pay)
+  (let ((person name))
+    (setf (get person 'name) name)
+    (setf (get person 'work-days) work-days)
+    (setf (get person 'daily-pay) daily-pay)
+    person))
+
+;; Make list
+(defun f3-make-list (my-list)
+  (let ((name) (work-days) (daily-pay))
+    (loop
+      (princ "Enter name or '0' to quit: ")
+      (setq name (read))
+      (when (equal name 0) (return))
+      (princ "Enter work days: ")
+      (setq work-days (read))
+      (princ "Enter daily pay: ")
+      (setq daily-pay (read))
+      (setq my-list (append my-list (list (f3-make-person name work-days daily-pay)))))))
+
+;; Get names of people with a certain daily-pay and work-days that are at least a certain amount
+(defun f3-print-people (input-list exact-daily-pay min-work-days)
+  (cond ((null input-list) nil)
+        ((and (equal (get (car input-list) 'daily-pay) exact-daily-pay)
+              (>= min-work-days (get (car input-list) 'work-days)))
+         (cons (get (car input-list) 'name) (f3-print-people (cdr input-list) exact-daily-pay min-work-days))) 
+        (t (f3-print-people (cdr input-list) exact-daily-pay min-work-days))
+  )
+)
