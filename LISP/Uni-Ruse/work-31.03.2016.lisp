7303b2b2417a2a0d0828a22e1197add2ea9a099d
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-03-31 09:09:52 +0300

    Update work-31.03.2016.lisp

--- LISP/Uni-Ruse/work-31.03.2016.lisp
+++ LISP/Uni-Ruse/work-31.03.2016.lisp
@@ -10,3 +10,13 @@
     (when (equal p 8) (return))
     (cond ((numberp p) (print (/ (* 4 p) (* 11 (- p 8)))))
           (t (princ "not number")))))
+
+;;;; 2.	Дефинирайте функция, която 
+;;;; - премахва от входния списък с произволни елементи, 
+;;;; - всички положителни двуцифрени числа.
+    
+(defun f2 (input)
+  (cond ((null input) nil)
+        ((and (numberp (car input)) (< 9 (car input) 100))
+         (f2 (cdr input)))
+        (t (cons (car input) (f2 (cdr input))))))
