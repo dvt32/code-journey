6846e07b93ef794ab5086f439037e6ad3d811574
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-20 11:43:43 +0300

    Create Matching Word & Non-Word Character.java

---
+++ Java/HackerRank/Matching Word & Non-Word Character.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-word-non-word
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\w\\w\\w\\W\\w\\w\\w\\w\\w\\w\\w\\w\\w\\w\\W\\w\\w\\w"); // Use \\ instead of using \ 
+    
+    }
+}
