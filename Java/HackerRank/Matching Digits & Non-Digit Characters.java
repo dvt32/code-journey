9e481fbfc19a6f50fc6d5e9f0ab678ed7e8138c6
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-20 11:38:39 +0300

    Create Matching Digits & Non-Digit Characters.java

---
+++ Java/HackerRank/Matching Digits & Non-Digit Characters.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-digits-non-digit-character
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\d\\d\\D\\d\\d\\D\\d\\d\\d\\d");
+    
+    }
+}
