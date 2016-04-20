46e7b05a0991eef046cb0de99522e6f0e788bba4
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-20 11:35:40 +0300

    Create Matching Anything But a Newline.java

---
+++ Java/HackerRank/Matching Anything But a Newline.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-anything-but-new-line
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("...\\....\\....\\...."); 
+    
+    }
+}
