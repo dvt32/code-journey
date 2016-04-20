75bfe9574a5601593217d8e3f309ff9194f8ab86
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-20 11:40:56 +0300

    Create Matching Whitespace & Non-Whitespace Character.java

---
+++ Java/HackerRank/Matching Whitespace & Non-Whitespace Character.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-whitespace-non-whitespace-character
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\S\\S\\s\\S\\S\\s\\S\\S");
+    
+    }
+}
