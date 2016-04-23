c7c0b7a6d77b6dd7f663e5cbd59e1dd0dce4d67c
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-23 14:01:33 +0300

    Create Matching {x} Repetitions.java

---
+++ Java/HackerRank/Matching {x} Repetitions.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-x-repetitions
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[a-zA-Z02468]{40}[13579\\s\\t]{5}$");
+    
+    }
+}
