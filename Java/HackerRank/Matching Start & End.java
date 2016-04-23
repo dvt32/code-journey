625d5dabd1e73d41cabc39e6d535a378ce130eff
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-23 13:36:18 +0300

    Create Matching Start & End.java

---
+++ Java/HackerRank/Matching Start & End.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-start-end
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d\\w\\w\\w\\w.$");
+    
+    }
+}
