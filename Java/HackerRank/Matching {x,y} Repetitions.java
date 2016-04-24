5b91df360c14c8ce2b1fb26859a835e1bd6ee979
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-24 21:12:53 +0300

    Create Matching {x,y} Repetitions.java

---
+++ Java/HackerRank/Matching {x,y} Repetitions.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-x-y-repititions
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d{1,2}[A-Za-z]{3,}[.]{0,3}$");
+    
+    }
+}
