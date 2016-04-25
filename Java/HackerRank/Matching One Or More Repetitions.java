c003fa9affb668ae0ed18c1cdbf4a946f0b845f7
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-25 15:23:13 +0300

    Create Matching One Or More Repetitions.java

---
+++ Java/HackerRank/Matching One Or More Repetitions.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-one-or-more-repititions
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[0-9]+[A-Z]+[a-z]+$"); // Use \\ instead of using \ 
+    
+    }
+}
