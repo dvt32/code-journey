760e46f1817096193e4da643cc77eb8050632c2a
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-25 15:39:00 +0300

    Create Matching Word Boundaries.java

---
+++ Java/HackerRank/Matching Word Boundaries.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-word-boundaries
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\b[AEIOUaeiou][A-Za-z]*\\b"); // Use \\ instead of using \ 
+    
+    }
+}
