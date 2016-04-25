8fc13ca274cde90912ef36f95642b9823a28d6cc
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-04-25 15:26:37 +0300

    Create Matching Ending Items.java

---
+++ Java/HackerRank/Matching Ending Items.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/matching-ending-items
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[A-Za-z]*s$"); // Use \\ instead of using \ 
+    
+    }
+}
