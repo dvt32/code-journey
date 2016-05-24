a44a8d6bbf659d3d3e1e806eb26c6431338affdb
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-05-24 20:11:30 +0300

    Create Multi.lisp

---
+++ LISP/Multi.lisp
@@ -0,0 +1,14 @@
+;;;; Да се дефинира функция, която намира
+;;;; произведението на елементите на даден списък, които са числа, по–
+;;;; малки от a.
+
+;; Define function
+(defun multi (input-list a)
+  (cond ((null input-list) 1)
+        ((and (numberp (car input-list))
+              (< (car input-list) a))
+          (* (car input-list) (multi (cdr input-list) a)))
+        (t (multi (cdr input-list) a))))
+
+;; Call function
+(multi '(1 2 3 4 a -5 2) 4)
