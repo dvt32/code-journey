6668ec844a59faeb849ab690db448de46379ba9e
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-07-19 14:07:26 +0300

    Create Weather Observation Station 5.sql

---
+++ SQL/HackerRank/Weather Observation Station 5.sql
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/weather-observation-station-5
+
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) ASC LIMIT 1;
+
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) DESC LIMIT 1;
