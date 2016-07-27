41b4fdd666ffda358bd0c26912408242d389eb53
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-07-27 11:05:17 +0300

    Create Java Singleton.java

---
+++ Java/HackerRank/Java Singleton.java
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/java-singleton
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+class Singleton {
+    private Singleton() {}
+    public String str;
+    private static Singleton instance = new Singleton();
+    public static Singleton getSingleInstance() { return instance; }
+}
