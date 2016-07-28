9013f9e78fa5ab0dd8fc1cdfa824adfa8137dba0
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-07-28 14:18:30 +0300

    Create Drop it.js

---
+++ JavaScript/FreeCodeCamp/Intermediate-Algorithm-Scripting/Drop it.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.com/challenges/drop-it
+
+function dropElements(arr, func) {
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift(); 
+  }
+  return arr;
+}
+
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
