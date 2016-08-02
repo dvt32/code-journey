fa2c585a112088dfb40ea1335913e7b3e42e370c
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-08-02 16:51:34 +0300

    Create Comparing Numbers.sh

---
+++ Bash/HackerRank/Comparing Numbers.sh
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/bash-tutorials---comparing-numbers
+
+read x
+read y
+
+if (( $x < $y ))
+    then echo "X is less than Y"
+elif (( $x > $y ))
+    then echo "X is greater than Y"
+else
+    echo "X is equal to Y"
+fi
