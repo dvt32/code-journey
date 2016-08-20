376f7871043bde8b9432901e8da6862d338d6529
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-08-20 17:37:58 +0300

    Create Arithmetic Operators.py

---
+++ Python/HackerRank/Arithmetic Operators.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators
+
+a = int(input())
+b = int(input())
+
+sum = a + b
+diff = a - b
+prod = a * b
+
+print(sum)
+print(diff)
+print(prod)
