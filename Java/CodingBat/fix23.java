df48f35899ca0bdb219f99d4610ea3f89316b6b2
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-10-15 12:17:12 +0300

    Create fix23.java

---
+++ Java/CodingBat/fix23.java
@@ -0,0 +1,10 @@
+// http://codingbat.com/prob/p120347
+
+public int[] fix23(int[] nums) {
+  for (int i = 0; i < nums.length-1; ++i) {
+    if (nums[i] == 2 && nums[i+1] == 3) {
+      nums[i+1] = 0;
+    }
+  }
+  return nums;
+}
