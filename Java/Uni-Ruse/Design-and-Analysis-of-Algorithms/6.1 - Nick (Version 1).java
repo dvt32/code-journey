c37a9be415ad685c7d9fde5a58a408ad836768b7
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-10-20 08:42:39 +0300

    Create 6.1 - Nick (Version 1).java

---
+++ Java/Uni-Ruse/Design-and-Analysis-of-Algorithms/6.1 - Nick (Version 1).java
@@ -0,0 +1,60 @@
+// dvt32 - 20.10.2016
+
+/*
+1.	Потребителско име (никнейм)
+Ето, че и сегашният никнейм: 5|<%P3_FFFE|\|G@+/\–lIgLic4hkayyy!!1! на скайп- фенката Лигличкaтъ взе да й омръзва. Не може и да я вините – само от 15 минути е в Skype-а, а някои от приятелите й вече я разпознаха (което всъщност трябваше да се очаква). Затова е нетърпелива да последва днешната мода – да си избере за никнейм някаква дълбока мисъл, изписана с редуващи се главни и малки букви: НеЩо–тАкОвА–СеЩаШ–Ли–сЕ–КоЛкО–Е–ЯкО. А това е изключително затормозяващо! Трябва постоянно да внимаваш и да помниш четността на поредния номер на текущата буква заради правилото:
+•	ако на нечетна позиция в ника има буква –  тя непременно трябва да е главна;
+•	ако на четна позиция в ника има буква – тя непременно трябва да е малка;
+•	цифрите и другите знаци, които не са букви –  не се променят, независимо на каква позиция са; 
+•	интервали и букви на кирилица в ника няма да има, нито пък точки, с изключение на една за край на цялото това велико умотворение.
+Напишете програма nick, която получава като вход мъдрата мисъл, която нашата героиня си е избрала за скайп-име и извежда същия текст, променен до неузнаваемост, както повелява скайп-модата. Позициите в текста броим от 1, започвайки от най-левия знак.
+
+ */
+
+// To optimize
+// - remove currentPosition, rather use i+1 to get the currentPosition
+// - use StringBuilder instead of String - saves some resources
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		
+		String input = read.nextLine();
+		String output = "";
+		
+		int currentPosition = 1;
+		for (int i = 0; i < input.length(); ++i) {
+			char currentChar = input.charAt(i);
+			boolean currentCharIsLetter = Character.isLetter(currentChar);
+			switch (currentPosition % 2) {
+				case 0:
+					if (currentCharIsLetter) {
+						output += Character.toString(Character.toLowerCase(currentChar));
+					}
+					else {
+						output += Character.toString(currentChar);
+					}
+					break;
+				case 1: 
+					if (currentCharIsLetter) {
+						output += Character.toString(Character.toUpperCase(currentChar));
+					}
+					else {
+						output += Character.toString(currentChar);
+					}
+					break;
+				default: break;
+			}
+			currentPosition++;
+		}
+		
+		// Print end result
+		System.out.println(output);
+		
+		// Close scanner
+		read.close();
+	}
+}
