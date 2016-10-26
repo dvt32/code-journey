12b061287489343bd1d12c79a94354a44044262b
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-10-26 09:32:21 +0300

    Create posNeg.java

---
+++ Java/CodingBat/posNeg.java
@@ -0,0 +1,10 @@
+// http://codingbat.com/prob/p159227
+
+public boolean posNeg(int a, int b, boolean negative) {
+  if (negative) {
+    return (a < 0 && b < 0);
+  }
+  else {
+    return ((a < 0 && b > 0) || (a > 0 && b < 0));
+  }
+}
