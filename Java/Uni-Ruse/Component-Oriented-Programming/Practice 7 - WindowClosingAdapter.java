15d5bed162a86cf9bf2c98ad7b292893526ddae7
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-10-26 08:29:26 +0300

    Create Practice 7 - WindowClosingAdapter.java

---
+++ Java/Uni-Ruse/Component-Oriented-Programming/Practice 7 - WindowClosingAdapter.java
@@ -0,0 +1,20 @@
+import java.awt.event.*;
+
+public class WindowClosingAdapter extends WindowAdapter {
+	private boolean exitSystem;
+	
+	public WindowClosingAdapter(boolean exitSystem) {
+		this.exitSystem = exitSystem;
+	}
+	
+	public WindowClosingAdapter() {
+		this(false);
+	}
+	public void windowClosing(WindowEvent event) {
+		event.getWindow().setVisible(false);
+		event.getWindow().dispose();
+		if (exitSystem) {
+			System.exit(0);
+		}
+	}
+}
