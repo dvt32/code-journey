a87d47c32a4c03d326b5ed87f5143bb92c69a425
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-10-27 08:52:32 +0300

    Create fibonacci.java

---
+++ Java/CodingBat/fibonacci.java
@@ -0,0 +1,12 @@
+// http://codingbat.com/prob/p120015
+
+public int fibonacci(int n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  
+  return fibonacci(n-1) + fibonacci(n-2);
+}
