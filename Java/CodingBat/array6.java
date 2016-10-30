f593302159c4954cc7d949043ab5197643b1c76b
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-10-30 20:46:47 +0200

    Create array6.java

---
+++ Java/CodingBat/array6.java
@@ -0,0 +1,14 @@
+// http://codingbat.com/prob/p108997
+
+public boolean array6(int[] nums, int index) {
+  if (index == nums.length) {
+    return false;
+  }
+  
+  if (nums[index] == 6) {
+    return true;
+  }
+  else {
+    return array6(nums, index+1);
+  }
+}
