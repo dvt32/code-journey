e6c2d62c3abc3b429d80b89ebb771473c47e75dc
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-02 20:17:19 +0200

    Create front22.java

---
+++ Java/CodingBat/front22.java
@@ -0,0 +1,10 @@
+// http://codingbat.com/prob/p183592
+
+public String front22(String str) {
+  if (str.length() < 2) {
+    return str + str + str;
+  }
+  else {
+    return str.substring(0, 2) + str + str.substring(0, 2);
+  }
+}
