b040c10ba07dbd4d28cbcfe3610c0b9fab4b2562
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-03 20:05:05 +0200

    Create in3050.java

---
+++ Java/CodingBat/in3050.java
@@ -0,0 +1,6 @@
+// http://codingbat.com/prob/p132134
+
+public boolean in3050(int a, int b) {
+  return ((a >= 30 && a <= 40 && b >= 30 && b <= 40) ||
+          (a >= 40 && a <= 50 && b >= 40 && b <= 50));
+}
