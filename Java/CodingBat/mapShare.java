8d70683b932eede1ba847b23d3bda0496abc894e
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-03 21:24:09 +0200

    Create mapShare.java

---
+++ Java/CodingBat/mapShare.java
@@ -0,0 +1,9 @@
+// http://codingbat.com/prob/p148813
+
+public Map<String, String> mapShare(Map<String, String> map) {
+  if (map.containsKey("a")) {
+    map.put("b", map.get("a"));
+  }
+  map.remove("c");
+  return map;
+}
