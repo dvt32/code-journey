9d8359ac343d3609d1a4ba782064add204c1d123
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-04 17:43:13 +0200

    Create topping2.java

---
+++ Java/CodingBat/topping2.java
@@ -0,0 +1,11 @@
+// http://codingbat.com/prob/p196458
+
+public Map<String, String> topping2(Map<String, String> map) {
+  if (map.containsKey("ice cream")) {
+    map.put("yogurt", map.get("ice cream"));
+  }
+  if (map.containsKey("spinach")) {
+    map.put("spinach", "nuts");
+  }
+  return map;
+}
