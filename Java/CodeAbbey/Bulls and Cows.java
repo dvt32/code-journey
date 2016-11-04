6d7059d175da45d71a4cfa4314bcd45058c3e60d
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-04 10:50:54 +0200

    Create Bulls and Cows.java

---
+++ Java/CodeAbbey/Bulls and Cows.java
@@ -0,0 +1,36 @@
+// http://www.codeabbey.com/index/task_view/bulls-and-cows
+
+import java.util.Scanner;
+ 
+class Solution
+{
+	
+    public static void main(String[] args)
+    {
+    	Scanner read = new Scanner(System.in);
+        
+    	String secretValue = read.next();
+    	int numberOfGuesses = read.nextInt();
+    	
+    	// Consume newline character
+    	read.nextLine();
+    	
+    	for (int i = 0; i < numberOfGuesses; ++i) {
+    		String guess = read.next();
+    		int numberOfCorrectlyPlacedDigits = 0;
+    		int numberOfIncorrectlyPlacedDigits = 0;
+    		for (int j = 0; j < secretValue.length(); ++j) {
+    			if (guess.charAt(j) == secretValue.charAt(j)) {
+    				numberOfCorrectlyPlacedDigits++;
+    			}
+    			else if (secretValue.contains(Character.toString(guess.charAt(j)))) {
+    				numberOfIncorrectlyPlacedDigits++;
+    			}
+    		}
+    		System.out.print(numberOfCorrectlyPlacedDigits + "-" + numberOfIncorrectlyPlacedDigits + " ");
+    	}
+    	
+        // Close scanner
+        read.close();
+    }
+}
