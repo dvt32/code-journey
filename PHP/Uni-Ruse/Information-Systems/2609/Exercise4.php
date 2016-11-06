4cd043583cc2dba9dea264ae551f88a2edd12261
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-06 21:44:39 +0200

    Create Exercise4.php

---
+++ PHP/Uni-Ruse/Information-Systems/2609/Exercise4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>26.09.2016 - dvt32</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<p style="text-align: center;">
+			<?php
+				$firstLine = '"<strong>University of Ruse</strong>", Ruse';
+				$secondLine = '"Studentska" Street N8';
+				echo $firstLine . "<br />" . $secondLine;
+			?>
+		</p>
+	</body>
+</html>
