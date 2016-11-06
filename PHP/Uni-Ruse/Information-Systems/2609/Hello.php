3356ca00d1a6f555c02a30438199acc803df8424
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-06 20:14:26 +0200

    Create Hello.php

---
+++ PHP/Uni-Ruse/Information-Systems/2609/Hello.php
@@ -0,0 +1,15 @@
+<?php
+	$txt ='
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>My first PHP program</title>
+				<meta charset="windows-1251">
+			</head>
+			<body>
+				<p style="color:red;">Hello world!</p>
+			</body>
+		</html>';
+		
+	print $txt;
+?>
