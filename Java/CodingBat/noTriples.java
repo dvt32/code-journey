ed16b2b7a2ae360ddcb8591427a137d410d0ac1d
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-10 08:35:32 +0200

    Create noTriples.java

---
+++ Java/CodingBat/noTriples.java
@@ -0,0 +1,13 @@
+// http://codingbat.com/prob/p170221
+
+public boolean noTriples(int[] nums) {
+  for (int i = 0; i < (nums.length-2); ++i) {
+    if (nums[i] == nums[i+1] &&
+        nums[i] == nums[i+2] &&
+        nums[i+1] == nums[i+2])
+    {
+      return false;
+    }
+  }
+  return true;
+}
