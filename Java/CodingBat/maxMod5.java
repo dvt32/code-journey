84c0a725a5c3e0a1ffc0a99e49bf10176ae8ff95
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-25 22:04:36 +0200

    Create maxMod5.java

---
+++ Java/CodingBat/maxMod5.java
@@ -0,0 +1,13 @@
+// http://codingbat.com/prob/p115384
+
+public int maxMod5(int a, int b) {
+  if (a == b) {
+    return 0;
+  }
+  else if (a % 5 == b % 5) {
+    return Math.min(a, b);
+  }
+  else {
+    return Math.max(a, b);
+  }
+}
