9c3e2fb4d13f4b75b148c6807db6c30a410ad1d3
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-30 09:22:17 +0200

    Create fizzArray.java

---
+++ Java/CodingBat/fizzArray.java
@@ -0,0 +1,11 @@
+// http://codingbat.com/prob/p180920
+
+public int[] fizzArray(int n) {
+  int[] arr = new int[n];
+  
+  for (int i = 0; i < arr.length; ++i) {
+    arr[i] = i;
+  }
+  
+  return arr;
+}
