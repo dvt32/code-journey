67ffa24c2dec7aa031f0ec97b51c07325d82a7e7
Dimitar Trifonov <dtrifonov32@gmail.com>
2016-11-30 08:13:23 +0200

    Create loneSum.java

---
+++ Java/CodingBat/loneSum.java
@@ -0,0 +1,17 @@
+// http://codingbat.com/prob/p148972
+
+public int loneSum(int a, int b, int c) {
+  int sum = 0;
+  
+  if (a != b && a != c) {
+    sum += a;
+  }
+  if (b != a && b != c) {
+    sum += b;
+  }
+  if (c != a && c != b) {
+    sum += c;
+  }
+  
+  return sum;
+}
