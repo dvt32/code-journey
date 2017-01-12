998ff67f38d30ede6b755da6fe8586f3e53c5f5a
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-01-12 14:52:55 +0200

    Create sumHeights.java

---
+++ Java/CodingBat/sumHeights.java
@@ -0,0 +1,11 @@
+// http://codingbat.com/prob/p148138
+
+public int sumHeights(int[] heights, int start, int end) {
+  int sum = 0;
+  
+  for (int i = start; i < end; ++i) {
+    sum += Math.abs(heights[i] - heights[i+1]);
+  }
+  
+  return sum;
+}
