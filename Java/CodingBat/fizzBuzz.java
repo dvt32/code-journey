455a83df13c4622e05b342a02319733fdac1fad5
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-01-19 11:39:12 +0200

    Create fizzBuzz.java

---
+++ Java/CodingBat/fizzBuzz.java
@@ -0,0 +1,23 @@
+// http://codingbat.com/prob/p153059
+
+public String[] fizzBuzz(int start, int end) {
+  String[] numbers = new String[end-start];
+  
+  for (int i = 0; i < numbers.length; ++i) {
+    int number = (start + i);
+    if (number % 3 == 0 && number % 5 == 0) {
+      numbers[i] = "FizzBuzz";
+    }
+    else if (number % 3 == 0) {
+      numbers[i] = "Fizz";
+    }
+    else if (number % 5 == 0) {
+      numbers[i] = "Buzz";
+    }
+    else {
+      numbers[i] = String.valueOf(number);
+    }
+  }
+  
+  return numbers;
+}
