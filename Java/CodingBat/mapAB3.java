e82c26ada509586735fa97c97931fb880deb22d4
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-03-31 00:44:30 +0300

    Create mapAB3.java

---
+++ Java/CodingBat/mapAB3.java
@@ -0,0 +1,14 @@
+// http://codingbat.com/prob/p115012
+
+public Map<String, String> mapAB3(Map<String, String> map) {
+  if ( map.containsKey("a") ^ map.containsKey("b") ) {
+    if (map.containsKey("a")) {
+      map.put( "b", map.get("a") );
+    }
+    else {
+      map.put( "a", map.get("b") );
+    }
+  }
+  
+  return map;
+}
