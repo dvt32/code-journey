f924221e23cd378b22a8ca53060d3183d96c239e
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-04-19 11:33:55 +0300

    Create monkey_trouble.py

---
+++ Python/CodingBat/monkey_trouble.py
@@ -0,0 +1,9 @@
+# http://codingbat.com/prob/p120546
+
+def monkey_trouble(a_smile, b_smile):
+  if a_smile == True and b_smile == True:
+    return True
+  elif a_smile == False and b_smile == False:
+    return True
+  else:
+    return False
