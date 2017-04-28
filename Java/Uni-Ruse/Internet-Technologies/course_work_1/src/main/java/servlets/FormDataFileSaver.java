c165337b899a065b7a8b09eba5a5f05cc7c1ab98
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-04-28 11:32:07 +0300

    Generate OUTPUT_FILE_PATH from ServletContext real path

--- Java/Uni-Ruse/Internet-Technologies/course_work_1/src/main/java/servlets/FormDataFileSaver.java
+++ Java/Uni-Ruse/Internet-Technologies/course_work_1/src/main/java/servlets/FormDataFileSaver.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,9 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 public class FormDataFileSaver extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	private final String OUTPUT_FILE_PATH = "output.txt";
-       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext context = request.getServletContext();
+		final String OUTPUT_FILE_PATH =  context.getRealPath("/") + "output.txt";
+		
 		// Read data from submitted form
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
