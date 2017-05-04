9ef0cfe06665aba6ce65a8e16a66df41ec855e23
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-05-04 13:13:47 +0300

    Create string_match.py

---
+++ Python/CodingBat/string_match.py
@@ -0,0 +1,10 @@
+# http://codingbat.com/prob/p182414
+
+def string_match(a, b):
+  count = 0
+  
+  for i in range( min(len(a),len(b)) - 1 ):
+    if a[i:i+2] == b[i:i+2]:
+      count += 1
+  
+  return count
