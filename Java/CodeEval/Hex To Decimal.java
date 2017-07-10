f85c69125cb63c586ff4c60e17bc5a024d2340ae
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-07-10 11:31:15 +0300

    Create Hex To Decimal.java

---
+++ Java/CodeEval/Hex To Decimal.java
@@ -0,0 +1,24 @@
+// https://www.codeeval.com/open_challenges/67/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File(args[0]);
+      
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+        String lineInFile;
+
+        while ( (lineInFile = bufferedReader.readLine()) != null ) {
+            lineInFile = lineInFile.trim();
+            
+            if (lineInFile.equals("")) {
+            	lineInFile = bufferedReader.readLine(); // escape Enter key press
+            }
+           
+            System.out.println( Integer.parseInt(lineInFile, 16) );
+        }
+        
+        bufferedReader.close();
+    }
+}
