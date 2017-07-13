007371709f1fad2e17f7c8925b0a77187089ec2a
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-07-13 11:33:07 +0300

    Create File Size.java

---
+++ Java/CodeEval/File Size.java
@@ -0,0 +1,11 @@
+// https://www.codeeval.com/open_challenges/26/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File(args[0]);
+        
+        System.out.println( inputFile.length() );
+    }
+}
