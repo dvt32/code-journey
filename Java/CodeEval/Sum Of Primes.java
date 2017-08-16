682b184e7a560575b1d27d38d45934d3a9867bd5
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-08-16 12:22:45 +0300

    Create Sum Of Primes.java

---
+++ Java/CodeEval/Sum Of Primes.java
@@ -0,0 +1,30 @@
+// https://www.codeeval.com/open_challenges/4/
+
+import java.io.*;
+
+public class Main {	
+	public static void main(String[] args) {
+		int sum = 0;
+		int primeCounter = 0;
+		int currentNumber = 2;
+		
+		while (primeCounter != 1000) {
+			boolean isPrime = true;
+			for (int i = 2; i <= Math.sqrt(currentNumber); ++i) {
+				if (currentNumber % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if (isPrime) {
+				sum += currentNumber;
+				primeCounter++;
+			}
+			
+			currentNumber++;
+		}
+		
+		System.out.println( sum );
+	}
+}
