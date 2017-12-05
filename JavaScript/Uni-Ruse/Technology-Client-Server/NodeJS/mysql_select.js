dfa442a66ab39e36814ad21e3380a3099d00b00a
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-12-05 14:59:05 +0200

    Add files via upload

---
+++ JavaScript/Uni-Ruse/Technology-Client-Server/NodeJS/mysql_select.js
@@ -0,0 +1,16 @@
+﻿var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "srv.iits.uni-ruse.bg",
+  user: "inf52",
+  password: "inf52pass",
+  database: "db52"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM student11", function(err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
