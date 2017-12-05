22e54fff2d71f8a635bfbd9efb687e0b3c64bf48
Dimitar Trifonov <dtrifonov32@gmail.com>
2017-12-05 15:22:56 +0200

    Add files via upload

---
+++ JavaScript/Uni-Ruse/Technology-Client-Server/NodeJS/mysql_insert.js
@@ -0,0 +1,18 @@
+﻿var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "srv.iits.uni-ruse.bg",
+  user: "inf52",
+  password: "inf52pass",
+  database: "db52"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO Employees (id, first, last, age) VALUES (1, 'Some', 'Guy', 22)";
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
