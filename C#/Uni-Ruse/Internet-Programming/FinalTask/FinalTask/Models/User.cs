9240f7249d106795e9d154f5ef6d13d320682b55
Dimitar Trifonov <dtrifonov32@gmail.com>
2018-01-17 02:12:31 +0200

    Add final task

---
+++ C#/Uni-Ruse/Internet-Programming/FinalTask/FinalTask/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalTask.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+}
