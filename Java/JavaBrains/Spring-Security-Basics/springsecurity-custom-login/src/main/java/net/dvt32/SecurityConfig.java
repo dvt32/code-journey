955655ac1a0a52e29be6fa73d1c65e6982a0a2b0
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-02-29 16:27:39 +0200

    Add files via upload

---
+++ Java/JavaBrains/Spring-Security-Basics/springsecurity-custom-login/src/main/java/net/dvt32/SecurityConfig.java
@@ -0,0 +1,23 @@
+package net.dvt32;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig 
+	extends WebSecurityConfigurerAdapter 
+{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll();
+	}
+
+}
---
+++ Java/JavaBrains/Spring-Security-Basics/springsecurity-custom-login/src/main/java/net/dvt32/SpringsecurityCustomLoginApplication.java
@@ -0,0 +1,13 @@
+package net.dvt32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityCustomLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityCustomLoginApplication.class, args);
+	}
+
+}
