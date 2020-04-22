6c645ba5e03effc74b1120d816013aea98fda87b
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-04-22 17:38:07 +0300

    Create 1331A - Is it rated.java

---
+++ Java/CodeForces/1331A - Is it rated.java
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/1331/D
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		
+		System.out.println("NO");
+		
+		// Close scanner
+		read.close();
+	}
+
+}
