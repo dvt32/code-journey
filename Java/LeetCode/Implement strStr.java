941574cddff052bec7cf65189a6219c0d01c4d6e
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-05-07 16:47:07 +0300

    Create Implement strStr.java

---
+++ Java/LeetCode/Implement strStr.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/implement-strstr
+
+class Solution {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+    
+}
