48b61426eed1fcaa194de4192397881faaf5d411
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-05-23 16:39:38 +0300

    Create Sqrtx.java

---
+++ Java/LeetCode/Sqrtx.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sqrtx/
+
+// Lol, well... it passes. :)
+
+class Solution {
+    
+    public int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+    
+}
