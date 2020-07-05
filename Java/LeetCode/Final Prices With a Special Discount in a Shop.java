2ab2b423319164ac08fa5cb5af099b2401c51dad
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-07-05 14:40:12 +0300

    Create Final Prices With a Special Discount in a Shop.java

---
+++ Java/LeetCode/Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+class Solution {
+
+    public int[] finalPrices(int[] prices) {
+        for (int i = 0; i < prices.length; ++i) {
+            for (int j = i+1; j < prices.length; ++j) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        
+        return prices;
+    }
+    
+}
