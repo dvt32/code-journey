02f60d76da33fb2d7bc2370dd809c015f914065e
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-08-11 13:53:14 +0300

    Create Reverse String.java

---
+++ Java/LeetCode/Reverse String.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/reverse-string/
+
+class Solution {
+
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length-1; i < j; ++i, --j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }    
+    }
+    
+}
