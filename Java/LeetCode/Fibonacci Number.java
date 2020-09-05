5e3a1778736e6331fca60113e55814d735a5f224
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-09-05 15:59:54 +0300

    Create Fibonacci Number.java

---
+++ Java/LeetCode/Fibonacci Number.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+
+    public int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        
+        return fib(N-1) + fib(N-2);
+    }
+    
+}
