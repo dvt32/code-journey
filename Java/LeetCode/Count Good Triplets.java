5dc5326d1aafe1c6f8e48e21b3b676ba6845e535
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-11-04 13:08:10 +0200

    Create Count Good Triplets.java

---
+++ Java/LeetCode/Count Good Triplets.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/count-good-triplets/
+
+class Solution {
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int numberOfGoodTriplets = 0;
+        
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = i+1; j < arr.length; ++j) {
+                for (int k = j+1; k < arr.length; ++k) {
+                    if (Math.abs(arr[i] - arr[j]) <= a &&
+                        Math.abs(arr[j] - arr[k]) <= b &&
+                        Math.abs(arr[i] - arr[k]) <= c ) 
+                    {
+                        numberOfGoodTriplets++;
+                    }
+                }
+            }
+        }
+        
+        return numberOfGoodTriplets;
+    }
+    
+}
