a73f180d45ab663cd59b764df91f4e14ffed3790
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-11-13 15:11:42 +0200

    Create N-Repeated Element in Size 2N Array.java

---
+++ Java/LeetCode/N-Repeated Element in Size 2N Array.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+
+class Solution {
+
+    public int[] sortedSquares(int[] A) {
+        for (int i = 0; i < A.length; ++i) {
+            A[i] = A[i] * A[i];
+        }
+        
+        Arrays.sort(A);
+        
+        return A;
+    }
+    
+}
