4267b262c9cebb125ffc7b00b74ee9b6cc6e0601
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-11-21 20:36:34 +0200

    Create Array Partition I.java

---
+++ Java/LeetCode/Array Partition I.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/array-partition-i/
+
+class Solution {
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int maxSum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            maxSum += nums[i];
+        }
+        
+        return maxSum;
+    }
+    
+}
