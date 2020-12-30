e715b5ffc6ad319bed97f2101a809fd00459a5d8
Dimitar Trifonov <dtrifonov32@gmail.com>
2020-12-30 13:28:07 +0200

    Create Contains Duplicate II.java

---
+++ Java/LeetCode/Contains Duplicate II.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/contains-duplicate-ii/
+
+class Solution {
+    
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = i+1; j < nums.length && j < i+1+k; ++j) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+}
