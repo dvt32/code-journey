5518fac229dd7d22952e88ab032825250b3ea50d
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-09 13:31:43 +0200

    Update countEvens.java

--- Java/CodingBat/countEvens.java
+++ Java/CodingBat/countEvens.java
@@ -9,3 +9,23 @@ public int countEvens(int[] nums) {
   }
   return evenNumbersCount;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int countEvens(int[] nums) {
+  if (nums.length == 0) {
+      return 0;
+  }
+
+  return countEvens(nums, 0);
+}
+
+public int countEvens(int[] nums, int i) {
+  if (i == nums.length-1) {
+    return (nums[i] % 2 == 0) ? 1 : 0;
+  }
+  
+  return ((nums[i] % 2 == 0) ? 1 : 0) + countEvens(nums, i+1);
+}
+*/
