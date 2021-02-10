5295b3452056bdeac0f08e4d494a976f8d7a07ea
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-10 14:21:50 +0200

    Update isEverywhere.java

--- Java/CodingBat/isEverywhere.java
+++ Java/CodingBat/isEverywhere.java
@@ -9,3 +9,26 @@ public boolean isEverywhere(int[] nums, int val) {
   
   return true;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean isEverywhere(int[] nums, int val) {
+  if (nums.length <= 1) {
+    return true;
+  }
+  
+  return isEverywhere(nums, val, 0);
+}
+
+public boolean isEverywhere(int[] nums, int val, int i) {
+  if (i == nums.length-2) {
+    return (nums[i] == val || nums[i+1] == val);
+  }
+  if (nums[i] != val && nums[i+1] != val) {
+    return false;
+  }
+  
+  return isEverywhere(nums, val, i+1);
+}
+*/
