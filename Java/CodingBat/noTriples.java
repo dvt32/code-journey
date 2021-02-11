690787bc6c8b2e703b39081b8f3642eee64c8e59
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-11 15:12:34 +0200

    Update noTriples.java

--- Java/CodingBat/noTriples.java
+++ Java/CodingBat/noTriples.java
@@ -11,3 +11,30 @@ public boolean noTriples(int[] nums) {
   }
   return true;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean noTriples(int[] nums) {
+  if (nums.length < 3) {
+    return true;
+  }
+  
+  return noTriples(nums, 0);
+}
+
+public boolean noTriples(int[] nums, int i) {
+  if (i == nums.length-2) {
+    return true;
+  }
+  
+  if (nums[i] == nums[i+1] &&
+      nums[i] == nums[i+2] &&
+      nums[i+1] == nums[i+2])
+  {
+    return false;
+  }
+  
+  return noTriples(nums, i+1);
+}
+*/
