af6285c0081dffa717950339c764a361e8b04fcf
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-12 14:36:43 +0200

    Update fizzArray.java

--- Java/CodingBat/fizzArray.java
+++ Java/CodingBat/fizzArray.java
@@ -9,3 +9,51 @@ public int[] fizzArray(int n) {
   
   return arr;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] fizzArray(int n) {
+  if (n == 0) {
+    return new int[0];
+  }
+  
+  int[] arr = new int[n];
+  
+  return fizzArray(arr, n, 0);
+}
+
+public int[] fizzArray(int[] arr, int n, int i) {
+  arr[i] = i;
+  
+  if (i == n-1) {
+    return arr;
+  }
+  
+  return fizzArray(arr, n, i+1);
+}
+*/
+
+// Alternative recursive solution
+
+/*
+public int[] fizzArray(int n) {
+  if (n == 0) {
+    return new int[0];
+  }
+  
+  int[] arr = new int[n];
+  
+  return fizzArray(arr, n);
+}
+
+public int[] fizzArray(int[] arr, int n) {
+  if (n == 0) {
+    return arr;
+  }
+  
+  arr[n-1] = n-1;
+  
+  return fizzArray(arr, n-1);
+}
+*/
