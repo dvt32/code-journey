71ebabecd07e508d3ee8354a62d6b326ed3e2ac5
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-13 13:56:07 +0200

    Update sum28.java

--- Java/CodingBat/sum28.java
+++ Java/CodingBat/sum28.java
@@ -10,3 +10,26 @@ public boolean sum28(int[] nums) {
   
   return (sum == 8);
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean sum28(int[] nums) {
+  if (nums.length == 0) {
+    return false;
+  }
+  return sum28(nums, 0, 0);
+}
+
+public boolean sum28(int[] nums, int i, int twoSum) {
+  if (i == nums.length) {
+    return (twoSum == 8);
+  }
+  
+  if (nums[i] == 2) {
+    twoSum += 2;
+  }
+  
+  return sum28(nums, i+1, twoSum);
+}
+*/
