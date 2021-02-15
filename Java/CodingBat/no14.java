0682be0c78b97a94c12e72fb24eb05d0c77e0096
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-15 15:02:16 +0200

    Update no14.java

--- Java/CodingBat/no14.java
+++ Java/CodingBat/no14.java
@@ -15,3 +15,27 @@ public boolean no14(int[] nums) {
   
   return (no1 || no4);
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean no14(int[] nums) {
+  if (nums.length == 0) {
+    return true;
+  }
+  
+  return noNum(nums, 0, 1) || noNum(nums, 0, 4);
+}
+
+public boolean noNum(int[] nums, int i, int num) {
+  if (i == nums.length) {
+    return true;
+  }
+  
+  if (nums[i] == num) {
+    return false;
+  }
+  
+  return noNum(nums, i+1, num);
+}
+*/
