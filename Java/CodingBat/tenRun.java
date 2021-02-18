889c78928929853e8220238030970732fd3a4222
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-19 01:54:48 +0200

    Update tenRun.java
    Add recursive solution

--- Java/CodingBat/tenRun.java
+++ Java/CodingBat/tenRun.java
@@ -14,3 +14,30 @@ public int[] tenRun(int[] nums) {
   
   return nums;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] tenRun(int[] nums) {
+  if (nums.length <= 1) {
+    return nums;
+  }
+  
+  return tenRun(nums, 0, -1);
+}
+
+public int[] tenRun(int[] nums, int i, int currentMultiple) {
+  if (i == nums.length) {
+    return nums;
+  }
+  
+  if ( nums[i] % 10 == 0 ) {
+    currentMultiple = nums[i];
+  }
+  else if (currentMultiple != -1) {
+    nums[i] = currentMultiple;
+  }
+  
+  return tenRun(nums, i+1, currentMultiple);
+}
+*/
