10d12d4fda76a716630c985ce8e329ad4e218422
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-18 15:09:23 +0200

    Update notAlone.java
    Add recursive solution

--- Java/CodingBat/notAlone.java
+++ Java/CodingBat/notAlone.java
@@ -12,3 +12,30 @@ public int[] notAlone(int[] nums, int val) {
   
   return nums;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] notAlone(int[] nums, int val) {
+  if (nums.length <= 1) {
+    return nums;
+  }
+  
+  return notAlone(nums, val, 1);
+}
+
+public int[] notAlone(int[] nums, int val, int i) {
+  if (i == nums.length-1) {
+    return nums;
+  }
+  
+  if (nums[i] == val &&
+      nums[i] != nums[i-1] &&
+      nums[i] != nums[i+1]) 
+  {
+     nums[i] = Math.max(nums[i-1], nums[i+1]);
+  }
+  
+  return notAlone(nums, val, i+1);
+}
+*/
