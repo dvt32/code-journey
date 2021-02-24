e53f3caf726a0a8de31a19d1abfd3a2a126a42cd
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-24 15:38:53 +0200

    Update scoresClump.java
    Add recursive solution

--- Java/CodingBat/scoresClump.java
+++ Java/CodingBat/scoresClump.java
@@ -12,3 +12,30 @@ public boolean scoresClump(int[] scores) {
   
   return false;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean scoresClump(int[] scores) {
+  if (scores.length <= 2) {
+    return false;
+  }
+  
+  return scoresClump(scores, 2);
+}
+
+public boolean scoresClump(int[] scores, int i) {
+  if (i == scores.length) {
+    return false;
+  }
+  
+  if (Math.abs(scores[i] - scores[i-1]) <= 2 &&
+      Math.abs(scores[i] - scores[i-2]) <= 2 &&
+      Math.abs(scores[i-1] - scores[i-2]) <= 2) 
+  {
+      return true;
+  }
+  
+  return scoresClump(scores, i+1);
+}
+*/
