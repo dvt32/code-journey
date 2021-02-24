26402a4e5e57af4b72bad8cc6d3af27efedcf25f
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-24 15:32:36 +0200

    Update scoresIncreasing.java
    Add recursive solution

--- Java/CodingBat/scoresIncreasing.java
+++ Java/CodingBat/scoresIncreasing.java
@@ -9,3 +9,23 @@ public boolean scoresIncreasing(int[] scores) {
   
   return true;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean scoresIncreasing(int[] scores) {
+  return scoresIncreasing(scores, 1);
+}
+
+public boolean scoresIncreasing(int[] scores, int i) {
+  if (i == scores.length) {
+    return true;
+  }
+  
+  if (scores[i] < scores[i-1]) {
+    return false;
+  }
+  
+  return scoresIncreasing(scores, i+1);
+}
+*/
