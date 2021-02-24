51df83c5b87cf73171bc270b4cc3d6f7b8d6f2c5
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-24 15:26:54 +0200

    Update scores100.java
    Add recursive solution

--- Java/CodingBat/scores100.java
+++ Java/CodingBat/scores100.java
@@ -9,3 +9,27 @@ public boolean scores100(int[] scores) {
   
   return false;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean scores100(int[] scores) {
+  if (scores.length <= 1) {
+    return false;
+  }
+  
+  return scores100(scores, 1);
+}
+
+public boolean scores100(int[] scores, int i) {
+  if (i == scores.length) {
+    return false;
+  }
+  
+  if (scores[i] == 100 && scores[i-1] == 100) {
+    return true;
+  }
+  
+  return scores100(scores, i+1);
+}
+*/
