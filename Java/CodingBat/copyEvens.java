b3b716084c56e2707a791ff2dc4fcddf910f6213
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-26 01:30:51 +0200

    Update copyEvens.java
    Add recursive solution

--- Java/CodingBat/copyEvens.java
+++ Java/CodingBat/copyEvens.java
@@ -16,3 +16,31 @@ public int[] copyEvens(int[] nums, int count) {
   
   return newArray;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] copyEvens(int[] nums, int count) {
+  int[] newArray = new int[count];
+  
+  if (count == 0) {
+    return newArray;
+  }
+  
+  return copyEvens(nums, count, newArray, 0, 0);
+}
+
+public int[] copyEvens(int[] nums, int count, int[] newArray, int i, int j) {
+  if (i == nums.length || count == 0) {
+    return newArray;
+  }
+  
+  if (nums[i] % 2 == 0) {
+    newArray[j] = nums[i];
+    j++;
+    count--;
+  }
+  
+  return copyEvens(nums, count, newArray, i+1, j);
+}
+*/
