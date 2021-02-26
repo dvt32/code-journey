0115a66e5021534dba8e5e467dbc5619f0f422ad
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-26 15:11:46 +0200

    Update twoTwo.java
    Add recursive solution

--- Java/CodingBat/twoTwo.java
+++ Java/CodingBat/twoTwo.java
@@ -12,3 +12,25 @@ public boolean twoTwo(int[] nums) {
   
   return true;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean twoTwo(int[] nums) {
+  return twoTwo(nums, 0);
+}
+
+public boolean twoTwo(int[] nums, int i) {
+  if (i == nums.length) {
+    return true;
+  }
+  
+  boolean twoToTheLeft = (i-1 >= 0 && nums[i-1] == 2);
+  boolean twoToTheRight = (i+1 < nums.length && nums[i+1] == 2);
+  if (nums[i] == 2 && !twoToTheLeft && !twoToTheRight) {
+    return false;
+  }
+  
+  return twoTwo(nums, i+1);
+}
+*/
