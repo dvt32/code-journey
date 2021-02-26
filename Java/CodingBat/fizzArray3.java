463a2f6b9b2c94e4c11209509058343a6cd6fcd3
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-26 15:25:48 +0200

    Update fizzArray3.java
    Add recursive solution

--- Java/CodingBat/fizzArray3.java
+++ Java/CodingBat/fizzArray3.java
@@ -12,3 +12,24 @@ public int[] fizzArray3(int start, int end) {
   
   return newArr;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] fizzArray3(int start, int end) {
+  int[] newArr = new int[end - start];
+  int numberToAdd = start;
+  return fizzArray3(newArr, numberToAdd, 0);
+}
+
+public int[] fizzArray3(int[] newArr, int numberToAdd, int i) {
+  if (i == newArr.length) {
+    return newArr;
+  }
+  
+  newArr[i] = numberToAdd;
+  numberToAdd++;
+  
+  return fizzArray3(newArr, numberToAdd, i+1);
+}
+*/
