f70cf4b0e9b3550cad5d287a37bf00ce59471869
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-27 14:47:08 +0200

    Update fizzArray2.java
    Add recursive solution

--- Java/CodingBat/fizzArray2.java
+++ Java/CodingBat/fizzArray2.java
@@ -9,3 +9,22 @@ public String[] fizzArray2(int n) {
   
   return arr;
 }
+
+// Recursive solution (for fun)
+
+/*
+public String[] fizzArray2(int n) {
+  String[] arr = new String[n];
+  return fizzArray2(arr, n);
+}
+
+public String[] fizzArray2(String[] arr, int n) {
+  if (n == 0) {
+    return arr;
+  }
+  
+  arr[n-1] = String.valueOf(n-1);
+  
+  return fizzArray2(arr, n-1);
+}
+*/
