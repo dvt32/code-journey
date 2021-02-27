9f9f8bd162e7878adcb050ea7fd9da88312991ea
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-27 14:52:10 +0200

    Update has22.java
    Add recursive solution

--- Java/CodingBat/has22.java
+++ Java/CodingBat/has22.java
@@ -9,3 +9,27 @@ public boolean has22(int[] nums) {
   
   return false;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean has22(int[] nums) {
+  if (nums.length <= 1) {
+    return false;
+  }
+  
+  return has22(nums, 0);
+}
+
+public boolean has22(int[] nums, int i) {
+  if (i == nums.length-1) {
+    return false;
+  }
+  
+  if (nums[i] == 2 && nums[i+1] == 2) {
+    return true;
+  }
+  
+  return has22(nums, i+1);
+}
+*/
