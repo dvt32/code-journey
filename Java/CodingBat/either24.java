8ba4fa3f616662e2752fb36b68b3e29441bc35ff
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-02-28 18:05:58 +0200

    Update either24.java
    Add recursive solution

--- Java/CodingBat/either24.java
+++ Java/CodingBat/either24.java
@@ -15,3 +15,30 @@ public boolean either24(int[] nums) {
   
   return (has_2_next_to_2 ^ has_4_next_to_4); // ^ is XOR - only one of the two must be true, in order for the return value to also be true
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean either24(int[] nums) {
+  if (nums.length < 2) {
+    return false;
+  }
+  
+  return either24(nums, false, false, 0);
+}
+
+public boolean either24(int[] nums, boolean has_2_next_to_2, boolean has_4_next_to_4, int i) {
+  if (i == nums.length-1) {
+    return (has_2_next_to_2 ^ has_4_next_to_4);
+  }
+  
+  if (nums[i] == nums[i+1] && nums[i] == 2) {
+    has_2_next_to_2 = true;
+  }
+  else if (nums[i] == nums[i+1] && nums[i] == 4) {
+    has_4_next_to_4 = true;
+  }
+  
+  return either24(nums, has_2_next_to_2, has_4_next_to_4, i+1);
+}
+*/
