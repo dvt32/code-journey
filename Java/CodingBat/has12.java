97b6c94a2937752f55fb1bae74ba1fd024d6e174
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-01 15:21:09 +0200

    Update has12.java
    Add recursive solution

--- Java/CodingBat/has12.java
+++ Java/CodingBat/has12.java
@@ -14,3 +14,30 @@ public boolean has12(int[] nums) {
   
   return false;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean has12(int[] nums) {
+  if (nums.length < 2) {
+    return false;
+  }
+  
+  return has12(nums, false, 0);
+}
+
+public boolean has12(int[] nums, boolean has1, int i) {
+  if (i == nums.length) {
+    return false;
+  }
+  
+  if (nums[i] == 1) {
+    has1 = true;
+  }
+  else if (nums[i] == 2 && has1) {
+    return true;
+  }
+  
+  return has12(nums, has1, i+1);
+}
+*/
