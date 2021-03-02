34c3255c653a6288adf69127b06c941c7150df57
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-02 18:23:55 +0200

    Update evenOdd.java
    Add recursive solution

--- Java/CodingBat/evenOdd.java
+++ Java/CodingBat/evenOdd.java
@@ -13,3 +13,27 @@ public int[] evenOdd(int[] nums) {
   
   return nums;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] evenOdd(int[] nums) {
+  return evenOdd(nums, 0);
+}
+
+public int[] evenOdd(int[] nums, int i) {
+  if (i == nums.length) {
+    return nums;
+  }
+  
+  if (nums[i] % 2 == 0) {
+    for (int j = i; j > 0; --j) {
+      int temp = nums[j];
+      nums[j] = nums[j-1];
+      nums[j-1] = temp;
+    }
+  }
+  
+  return evenOdd(nums, i+1);
+}
+*/
