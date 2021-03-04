3997eebe28a7f5de50e443516fc90f7e0dc0fb40
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-04 15:02:45 +0200

    Update sumHeights2.java
    Add recursive solution

--- Java/CodingBat/sumHeights2.java
+++ Java/CodingBat/sumHeights2.java
@@ -14,3 +14,21 @@ public int sumHeights2(int[] heights, int start, int end) {
   
   return sum;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int sumHeights2(int[] heights, int start, int end) {
+  if (start == end) {
+    return 0;
+  }
+  
+  int i = start;
+  int sum = Math.abs(heights[i] - heights[i+1]);
+  if (heights[i] < heights[i+1]) {
+    sum *= 2;
+  }
+  
+  return sum + sumHeights2(heights, start+1, end);
+}
+*/
