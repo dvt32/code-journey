2efcff9828e34db6b247ef2712e08d2f0b666049
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-07 04:24:55 +0200

    Update bigDiff.java
    Add recursive solution

--- Java/CodingBat/bigDiff.java
+++ Java/CodingBat/bigDiff.java
@@ -13,3 +13,28 @@ public int bigDiff(int[] nums) {
   }
   return Math.abs(max-min);
 }
+
+// Recursive solution (for fun)
+
+/*
+public int bigDiff(int[] nums) {
+  int min = nums[0];
+  int max = nums[0];
+  return bigDiff(nums, min, max, 0);
+}
+
+public int bigDiff(int[] nums, int min, int max, int i) {
+  if (i == nums.length) {
+    return Math.abs(max-min);
+  }
+  
+  if (nums[i] > max) {
+    max = nums[i];
+  }
+  else if (nums[i] < min) {
+    min = nums[i];
+  }
+  
+  return bigDiff(nums, min, max, i+1);
+}
+*/
