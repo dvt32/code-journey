3e5984c3ad13c2b0e061aa086eda39d2697eb0a2
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-08 20:35:19 +0200

    Update matchUp-array2.java
    Add recursive solution

--- Java/CodingBat/matchUp-array2.java
+++ Java/CodingBat/matchUp-array2.java
@@ -12,3 +12,24 @@ public int matchUp(int[] nums1, int[] nums2) {
   
   return count;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int matchUp(int[] nums1, int[] nums2) {
+  return matchUp(nums1, nums2, 0);
+}
+
+public int matchUp(int[] nums1, int[] nums2, int i) {
+  if (i == nums1.length) {
+    return 0;
+  }
+  
+  int difference = Math.abs(nums1[i] - nums2[i]);
+  if (difference != 0 && difference <= 2) {
+    return 1 + matchUp(nums1, nums2, i+1);
+  }
+  
+  return matchUp(nums1, nums2, i+1);
+}
+*/
