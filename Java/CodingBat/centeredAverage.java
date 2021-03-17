90bbadcae3b383e59a1e41cfa9c1f5732344a554
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-17 15:32:00 +0200

    Update centeredAverage.java
    Add recursive solution

--- Java/CodingBat/centeredAverage.java
+++ Java/CodingBat/centeredAverage.java
@@ -29,3 +29,54 @@ public int centeredAverage(int[] nums) {
   int centeredAverage = sum / numbersCount;
   return centeredAverage;
 }
+
+// Recursive solution (for fun)
+// TODO: Refactor recursive method to use less parameters
+
+/*
+public int centeredAverage(int[] nums) {
+  int min = nums[0];
+  int max = nums[0];
+  int minIndex = 0;
+  int maxIndex = 0;
+  int sum = 0;
+  int numbersCount = 0;
+  return centeredAverage(nums, min, max, minIndex, maxIndex, sum, numbersCount, 0, 0);
+}
+
+public int centeredAverage(
+  int[] nums,
+  int min,
+  int max,
+  int minIndex,
+  int maxIndex,
+  int sum,
+  int numbersCount,
+  int i,
+  int j)
+{
+  if (i < nums.length) {
+    if (nums[i] > max) {
+      max = nums[i];
+      maxIndex = i;
+    }
+    else if (nums[i] < min) {
+      min = nums[i];
+      minIndex = i;
+    }
+    return centeredAverage(nums, min, max, minIndex, maxIndex, sum, numbersCount, i+1, j);
+  }
+  
+  if (j < nums.length) {
+    if (j != maxIndex && j != minIndex) {
+      sum += nums[j];
+      numbersCount++;
+    }
+    return centeredAverage(nums, min, max, minIndex, maxIndex, sum, numbersCount, i, j+1);
+  }
+  
+  int centeredAverage = sum / numbersCount;
+  return centeredAverage;
+}
+*/
+
