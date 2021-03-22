5e28cc0fbbe55d2773ce8dc9c0d572748568adf5
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-23 00:10:53 +0200

    Update post4.java
    Add recursive solution

--- Java/CodingBat/post4.java
+++ Java/CodingBat/post4.java
@@ -18,3 +18,35 @@ public int[] post4(int[] nums) {
   
   return newArray;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] post4(int[] nums) {
+  int lastFourIndex = getLastFourIndex(nums, nums.length-1);
+  int[] newArray = new int[nums.length - lastFourIndex - 1];
+  return getModifiedArr(newArray, nums, 0, lastFourIndex + 1);
+}
+
+public int getLastFourIndex(int[] nums, int i) {
+  if (i == 0) {
+    return 0;
+  }
+  
+  if (nums[i] == 4) {
+    return i;
+  }
+  
+  return getLastFourIndex(nums, i-1);
+}
+
+public int[] getModifiedArr(int[] newArr, int[] nums, int i, int j) {
+  if (i == newArr.length) {
+    return newArr;
+  }
+  
+  newArr[i] = nums[j];
+  
+  return getModifiedArr(newArr, nums, i+1, j+1);
+}
+*/
