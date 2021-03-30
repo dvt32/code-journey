e3240b48c452942a7377a3c56ba824062b4d988b
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-03-30 19:25:22 +0300

    Update repeatEnd.java
    Add recursive solution

--- Java/CodingBat/repeatEnd.java
+++ Java/CodingBat/repeatEnd.java
@@ -7,3 +7,20 @@ public String repeatEnd(String str, int n) {
   }
   return output.toString();
 }
+
+// Recursive solution (for fun)
+
+/*
+public String repeatEnd(String str, int n) {
+  return repeatEnd(str, n, 0);
+
+}
+
+public String repeatEnd(String str, int n, int i) {
+  if (i == n) {
+    return "";
+  }
+  
+  return str.substring( str.length()-n ) + repeatEnd(str, n, i+1);
+}
+*/
