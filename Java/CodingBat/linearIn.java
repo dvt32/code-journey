9682a6374110f721690acb058b3ec11bc7d6e444
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-03 15:06:30 +0300

    Update linearIn.java
    Add recursive solution

--- Java/CodingBat/linearIn.java
+++ Java/CodingBat/linearIn.java
@@ -24,3 +24,71 @@ public boolean linearIn(int[] outer, int[] inner) {
   
   return false;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean linearIn(int[] outer, int[] inner) {
+  if (inner.length == 0) {
+    return true;
+  }
+  
+  int foundElementsCount = 0;
+  int i = 0;
+  int j = 0;
+  
+  return linearIn(outer, inner, foundElementsCount, i, j);
+}
+
+public boolean linearIn(int[] outer, int[] inner, int foundElementsCount, int i, int j) {
+  if (i == outer.length) {
+    return false;
+  }
+  
+  if (outer[i] == inner[j]) {
+    foundElementsCount++;
+    j++;
+  }
+  else if (outer[i] > inner[j]) {
+    return false;
+  }
+  
+  if (foundElementsCount == inner.length) {
+    return true;
+  }
+  
+  return linearIn(outer, inner, foundElementsCount, i+1, j);
+}
+*/
+
+// Alternative shorter recursive solution
+
+/*
+public boolean linearIn(int[] outer, int[] inner) {
+  if (inner.length == 0) {
+    return true;
+  }
+  
+  int foundElementsCount = 0;
+  int i = 0;
+  int j = 0;
+  
+  return linearIn(outer, inner, foundElementsCount, i, j);
+}
+
+public boolean linearIn(int[] outer, int[] inner, int foundElementsCount, int i, int j) {
+  if (i == outer.length || outer[i] > inner[j]) {
+    return false;
+  }
+  
+  if (outer[i] == inner[j]) {
+    foundElementsCount++;
+    j++;
+    if (foundElementsCount == inner.length) {
+      return true;
+    }
+  }
+  
+  return linearIn(outer, inner, foundElementsCount, i+1, j);
+}
+*/
