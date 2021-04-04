13fee1b1778ae06304433df63a30cc18df44901d
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-04 16:19:19 +0300

    Update sum67.java
    Add recursive solution

--- Java/CodingBat/sum67.java
+++ Java/CodingBat/sum67.java
@@ -15,3 +15,30 @@ public int sum67(int[] nums) {
   
   return sum;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int sum67(int[] nums) {
+  if (nums.length == 0) {
+    return 0;
+  }
+  
+  return sum67(nums, 0);
+}
+
+public int sum67(int[] nums, int i) {
+  if (i == nums.length) {
+    return 0;
+  }
+  
+  if (nums[i] == 6) {
+    while (nums[i] != 7) {
+      i++;
+    }
+    return sum67(nums, i+1);
+  }
+  
+  return nums[i] + sum67(nums, i+1);
+}
+*/
