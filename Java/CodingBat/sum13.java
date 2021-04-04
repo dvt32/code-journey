0f4d58aa8cb0674ca5681e22c066cd58277bf155
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-04 16:11:57 +0300

    Update sum13.java
    Add recursive solution

--- Java/CodingBat/sum13.java
+++ Java/CodingBat/sum13.java
@@ -18,3 +18,27 @@ public int sum13(int[] nums) {
     return sum;
   }
 }
+
+// Recursive solution (for fun)
+
+/*
+public int sum13(int[] nums) {
+  if (nums.length == 0) {
+    return 0;
+  }
+  
+  return sum13(nums, 0);
+}
+
+public int sum13(int[] nums, int i) {
+  if (i >= nums.length) {
+    return 0;
+  }
+  
+  if (nums[i] == 13) {
+    return sum13(nums, i+2);
+  }
+  
+  return nums[i] + sum13(nums, i+1);
+}
+*/
