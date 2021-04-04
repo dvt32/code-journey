e302b966ba63ea1bd5baca32e158dc1817df00f6
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-04 16:04:30 +0300

    Update shiftLeft.java
    Add recursive solution

--- Java/CodingBat/shiftLeft.java
+++ Java/CodingBat/shiftLeft.java
@@ -9,3 +9,23 @@ public int[] shiftLeft(int[] nums) {
   
   return nums;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] shiftLeft(int[] nums) {
+  return shiftLeft(nums, 0);
+}
+
+public int[] shiftLeft(int[] nums, int i) {
+  if (i >= nums.length-1) {
+    return nums;
+  }
+  
+  int temp = nums[i];
+  nums[i] = nums[i+1];
+  nums[i+1] = temp;
+  
+  return shiftLeft(nums, i+1);
+}
+*/
