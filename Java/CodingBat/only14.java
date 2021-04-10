dbc0aefc5ced14b2abe10b9981b8a016ceb62d18
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-10 14:05:41 +0300

    Update only14.java
    Add recursive solution

--- Java/CodingBat/only14.java
+++ Java/CodingBat/only14.java
@@ -9,3 +9,23 @@ public boolean only14(int[] nums) {
   
   return true;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean only14(int[] nums) {
+  return only14(nums, 0);
+}
+
+public boolean only14(int[] nums, int i) {
+  if (i == nums.length) {
+    return true;
+  }
+  
+  if (nums[i] != 1 && nums[i] != 4) {
+    return false;
+  }
+  
+  return only14(nums, i+1);
+}
+*/
