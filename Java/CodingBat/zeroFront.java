aafb2f63a53d9dd7f1eef43e6463622180bb3267
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-10 14:30:25 +0300

    Update zeroFront.java

--- Java/CodingBat/zeroFront.java
+++ Java/CodingBat/zeroFront.java
@@ -15,3 +15,35 @@ public int[] zeroFront(int[] nums) {
   
   return nums;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] zeroFront(int[] nums) {
+  return zeroFront(nums, 0);
+}
+
+public int[] zeroFront(int[] nums, int i) {
+  if (i == nums.length) {
+    return nums;
+  }
+  
+  if (nums[i] == 0) {
+    int zeroIndex = i;
+    alterNums(nums, zeroIndex, i);
+  }
+  
+  return zeroFront(nums, i+1);
+}
+
+public void alterNums(int[] nums, int zeroIndex, int i) {
+  if (zeroIndex > 0 && nums[zeroIndex-1] != 0) {
+    int temp = nums[zeroIndex-1];
+    nums[zeroIndex-1] = 0;
+    nums[zeroIndex] = temp;
+    zeroIndex--;
+    
+    alterNums(nums, zeroIndex, i);
+  }
+}
+*/
