9e735b7c94ec81f11f28e7529593424899bf26f3
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-11 16:42:20 +0300

    Update fizzBuzz.java
    Add recursive solution

--- Java/CodingBat/fizzBuzz.java
+++ Java/CodingBat/fizzBuzz.java
@@ -21,3 +21,34 @@ public String[] fizzBuzz(int start, int end) {
   
   return numbers;
 }
+
+// Recursive solution (for fun)
+
+/*
+public String[] fizzBuzz(int start, int end) {
+  String[] numbers = new String[end-start];
+  return fizzBuzz(numbers, start, 0);
+}
+
+public String[] fizzBuzz(String[] numbers, int start, int i) {
+  if (i == numbers.length) {
+    return numbers;
+  }
+  
+  int number = (start + i);
+  if (number % 3 == 0 && number % 5 == 0) {
+    numbers[i] = "FizzBuzz";
+  }
+  else if (number % 3 == 0) {
+    numbers[i] = "Fizz";
+  }
+  else if (number % 5 == 0) {
+    numbers[i] = "Buzz";
+  }
+  else {
+    numbers[i] = String.valueOf(number);
+  }
+  
+  return fizzBuzz(numbers, start, i+1);
+}
+*/
