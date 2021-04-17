aad66b9100199484d6b1f12b9ed8ee08a10e0c19
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-17 13:02:45 +0300

    Update fix34.java
    Add recursive solution

--- Java/CodingBat/fix34.java
+++ Java/CodingBat/fix34.java
@@ -15,3 +15,34 @@ public int[] fix34(int[] nums) {
   
   return nums;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int[] fix34(int[] nums) {
+  return fix34(nums, 0);
+}
+
+public int[] fix34(int[] nums, int i) {
+  if (i == nums.length) {
+    return nums;
+  }
+
+  if (nums[i] == 3) {
+    int temp = nums[i+1];
+    nums[i+1] = 4;
+    assignTempValue(nums, i+2, temp);
+  }
+  
+  return fix34(nums, i+1);
+}
+
+public void assignTempValue(int[] nums, int j, int temp) {
+  if (j < nums.length) {
+    if (nums[j] == 4) {
+      nums[j] = temp;
+    }
+    assignTempValue(nums, j+1, temp);
+  }
+}
+*/
