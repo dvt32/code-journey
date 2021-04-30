b38fa9effe2e1f5fc52ba316333e89c4cdec1849
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-04-30 13:04:18 +0300

    Update maxMirror.java
    Add recursive solution

--- Java/CodingBat/maxMirror.java
+++ Java/CodingBat/maxMirror.java
@@ -24,3 +24,37 @@ public int maxMirror(int[] nums) {
                                                                                             
     return maxMirrorLength;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int maxMirror(int[] nums) {
+  int maxMirrorLength = 0;
+  return maxMirror(nums, maxMirrorLength, 0, nums.length-1);
+}
+
+public int maxMirror(int[] nums, int maxMirrorLength, int i, int j) {
+  if (i == nums.length) {
+    return maxMirrorLength;
+  }
+  
+  if (j >= 0) {
+      int mirrorLength = calcMirrorLength(nums, i, j);
+      maxMirrorLength = Math.max(maxMirrorLength, mirrorLength);
+      return maxMirror(nums, maxMirrorLength, i, j-1);
+  }
+  
+  return maxMirror(nums, maxMirrorLength, i+1, nums.length-1);
+}
+
+public int calcMirrorLength(int[] nums, int numberSequenceIndex, int mirrorSequenceIndex) {
+  if (numberSequenceIndex < nums.length && 
+      mirrorSequenceIndex >= 0 && 
+      nums[numberSequenceIndex] == nums[mirrorSequenceIndex])
+  {
+    return 1 + calcMirrorLength(nums, numberSequenceIndex+1, mirrorSequenceIndex-1);
+  }
+  
+  return 0;
+}
+*/
