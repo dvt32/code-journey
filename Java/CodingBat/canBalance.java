07448feb9cd3e5b8215a1c625a2a04ccf37bbfef
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-05-04 00:09:33 +0300

    Update canBalance.java
    Add recursive solution

--- Java/CodingBat/canBalance.java
+++ Java/CodingBat/canBalance.java
@@ -18,3 +18,34 @@ public boolean canBalance(int[] nums) {
   
   return false;
 }
+
+// Recursive solution (for fun)
+
+/*
+public boolean canBalance(int[] nums) {
+  return canBalance(nums, 0, 0);
+}
+
+public boolean canBalance(int[] nums, int sumSideA, int i) {
+  if (i == nums.length) {
+    return false;
+  }
+  
+  sumSideA += nums[i];
+  int sumSideB = getSumSideB(nums, i+1);
+  
+  if (sumSideA == sumSideB) {
+    return true;
+  }
+  
+  return canBalance(nums, sumSideA, i+1);
+}
+
+public int getSumSideB(int[] nums, int j) {
+  if (j == nums.length) {
+    return 0;
+  }
+  
+  return nums[j] + getSumSideB(nums, j+1);
+}
+*/
