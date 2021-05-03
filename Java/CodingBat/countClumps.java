1790a1418c9dcaf5c41d47a67bf7f65ec60bfa81
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-05-03 23:51:32 +0300

    Update countClumps.java
    Add recursive solution

--- Java/CodingBat/countClumps.java
+++ Java/CodingBat/countClumps.java
@@ -18,3 +18,29 @@ public int countClumps(int[] nums) {
   
   return clumpCount;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int countClumps(int[] nums) {
+  return countClumps(nums, 0, 1, 1);
+}
+
+public int countClumps(int[] nums, int clumpCount, int clumpLength, int i) {
+  if (i >= nums.length) {
+    return clumpCount;
+  }
+  
+  if (nums[i] == nums[i-1]) {
+    if (clumpLength < 2) {
+      clumpCount++;
+      clumpLength++;
+    }
+  }
+  else {
+    clumpLength = 1;
+  }
+  
+  return countClumps(nums, clumpCount, clumpLength, i+1);
+}
+*/
