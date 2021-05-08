387c3fb0eada428fb5f57e9d68d03ab3c6f0eeb1
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-05-08 12:59:30 +0300

    Update maxSpan.java
    Add recursive solution

--- Java/CodingBat/maxSpan.java
+++ Java/CodingBat/maxSpan.java
@@ -22,3 +22,37 @@ public int maxSpan(int[] nums) {
   
   return maxSpan;
 }
+
+// Recursive solution (for fun)
+
+/*
+public int maxSpan(int[] nums) {
+  int maxSpan = 0;
+  return maxSpan(nums, 0, 0, nums.length-1);
+}
+
+public int maxSpan(int[] nums, int maxSpan, int i, int j) {
+  if (i == nums.length) {
+    return maxSpan;
+  }
+  
+  if (nums[i] != nums[j]) {
+    return maxSpan(nums, maxSpan, i, j-1);
+  }
+  
+  int span = calcSpanLength(i, j);
+  if (span > maxSpan) {
+    maxSpan = span;
+  }
+  
+  return maxSpan(nums, maxSpan, i+1, nums.length-1);
+}
+
+public int calcSpanLength(int spanStartIndex, int j) {
+  if (spanStartIndex > j) {
+    return 0;
+  }
+  
+  return 1 + calcSpanLength(spanStartIndex+1, j);
+}
+*/
