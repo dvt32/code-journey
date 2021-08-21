9135c8015735cacd9f3ee7935b63335e4b9a8f18
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-08-21 13:10:24 +0300

    Create Island Perimeter.java

---
+++ Java/LeetCode/Island Perimeter.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/island-perimeter/
+
+class Solution {
+
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[i].length; ++j) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    if (i-1 >= 0 && grid[i-1][j] == 1) { perimeter--; }
+                    if (j-1 >= 0 && grid[i][j-1] == 1) { perimeter--; }
+                    if (i+1 < grid.length && grid[i+1][j] == 1) { perimeter--; }
+                    if (j+1 < grid[i].length && grid[i][j+1] == 1) { perimeter--; }
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+    
+}
