14d6b8f3e9748f0d52855d3d135023b9412f94c1
Dimitar Trifonov <dtrifonov32@gmail.com>
2021-11-07 18:55:29 +0200

    Create Multiply Strings.java

---
+++ Java/LeetCode/Multiply Strings.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/multiply-strings/
+
+import java.math.BigInteger;
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        return new BigInteger(num1).multiply(new BigInteger(num2)).toString();
+    }
+}
