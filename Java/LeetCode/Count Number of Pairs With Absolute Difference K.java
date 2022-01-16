1681d6e361f225e0518fe718bf7b27d38ba66655
Dimitar Trifonov <dtrifonov32@gmail.com>
2022-01-16 13:44:13 +0200

    Update Count Number of Pairs With Absolute Difference K.java

--- Java/LeetCode/Count Number of Pairs With Absolute Difference K.java
+++ Java/LeetCode/Count Number of Pairs With Absolute Difference K.java
@@ -6,8 +6,8 @@ class Solution {
         int pairCount = 0;
         
         for (int i = 0; i < nums.length; ++i) {
-            for (int j = 0; j < nums.length; ++j) {
-                if ( i < j && Math.abs(nums[i]-nums[j]) == k ) {
+            for (int j = i+1; j < nums.length; ++j) {
+                if ( Math.abs(nums[i]-nums[j]) == k ) {
                     pairCount++;
                 }
             }
