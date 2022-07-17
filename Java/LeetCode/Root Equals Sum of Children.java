1fbcf2cedf1545492d1559d813e06673e5369f8b
Dimitar Trifonov <dtrifonov32@gmail.com>
2022-07-17 13:40:50 +0300

    Create Root Equals Sum of Children.java

---
+++ Java/LeetCode/Root Equals Sum of Children.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/root-equals-sum-of-children/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        return root.val == (root.left.val + root.right.val);
+    }
+}
