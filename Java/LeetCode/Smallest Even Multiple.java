5f5a206dc039866865968d75fdab4cd8a5cbd14a
Dimitar Trifonov <dtrifonov32@gmail.com>
2022-10-09 14:55:48 +0300

    Update Smallest Even Multiple.java

--- Java/LeetCode/Smallest Even Multiple.java
+++ Java/LeetCode/Smallest Even Multiple.java
@@ -1,15 +1,9 @@
 // https://leetcode.com/problems/smallest-even-multiple
 
 class Solution {
-
+    
     public int smallestEvenMultiple(int n) {
-        int result = n;
-
-        while (result % 2 != 0) {
-            result *= 2;
-        }
-
-        return result;
+        return (n % 2 == 0) ? n : n*2;
     }
     
 }
