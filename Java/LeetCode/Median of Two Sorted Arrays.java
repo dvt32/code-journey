43e8bd2bf5301fb2d999acd0a8c204dd7a40fc62
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-01-06 12:04:35 +0200

    Update Median of Two Sorted Arrays.java

--- Java/LeetCode/Median of Two Sorted Arrays.java
+++ Java/LeetCode/Median of Two Sorted Arrays.java
@@ -20,4 +20,24 @@ class Solution {
         
         return (double) (nums[nums.length/2] + nums[nums.length/2-1]) / 2.0;
     }
+    
+    // Alternative solution
+    /*
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Integer> nums = new ArrayList<>();
+
+        for (int num : nums1) { nums.add(num); }
+        for (int num : nums2) { nums.add(num); }
+
+        Collections.sort(nums);
+
+        int size = nums.size();
+        if (size % 2 == 1) {
+            return (double) nums.get(size/2);
+        }
+
+        return (double) (nums.get(size/2) + nums.get(size/2-1)) / 2.0d;
+    }
+    */
+    
 }
