43313c8728adf4fbb25805089dc972ce049bed74
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-04-22 23:33:20 +0300

    Create Counter.js

---
+++ JavaScript/LeetCode/Counter.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/counter
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    var counter = n;
+    return function() {
+        return counter++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
