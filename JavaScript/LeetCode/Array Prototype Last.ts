2a9492485b33911ef67f6319e38c39a81e0c66e9
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-04-23 16:48:46 +0300

    Create Array Prototype Last.ts

---
+++ JavaScript/LeetCode/Array Prototype Last.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/array-prototype-last
+
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function() {
+    if (this.length > 0) {
+        return this[this.length-1];
+    }
+    
+    return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
