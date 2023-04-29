aca391645d2ecf8511bb34004cbe514472725580
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-04-30 02:14:43 +0300

    Create Function Composition.ts

---
+++ JavaScript/LeetCode/Function Composition.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/function-composition
+
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    return function(x) {
+        for (var i = functions.length-1; i >= 0; --i) {
+            x = functions[i](x)
+        }
+
+        return x
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
