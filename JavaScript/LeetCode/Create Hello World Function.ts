304b4b48287ffa819c39f87fccdb5ff3c0eecf99
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-05-07 15:39:41 +0300

    Create Create Hello World Function.ts

---
+++ JavaScript/LeetCode/Create Hello World Function.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/create-hello-world-function
+
+function createHelloWorld() {
+	return function(...args): string {
+        return "Hello World";
+    };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
