99f90ddcbbfa25ced5f0ed0548d601b88f9a0a39
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-06-25 14:28:16 +0300

    Create Array Wrapper.js

---
+++ JavaScript/LeetCode/Array Wrapper.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/array-wrapper
+
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((currentSum, num) => currentSum + num, 0);
+}
+
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.nums)
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
