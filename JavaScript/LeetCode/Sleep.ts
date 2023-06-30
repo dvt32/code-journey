2cab94f18f990840108a0260af5900bc4c6a5a30
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-06-30 12:43:26 +0300

    Create Sleep.ts

---
+++ JavaScript/LeetCode/Sleep.ts
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sleep
+
+async function sleep(millis: number): Promise<void> {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
