981f9451300282941c9dd27661215d74daff0031
Dimitar Trifonov <dtrifonov32@gmail.com>
2023-09-16 18:45:58 +0300

    Create Count Prefixes of a Given String.java

---
+++ Java/LeetCode/Count Prefixes of a Given String.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/count-prefixes-of-a-given-string
+
+class Solution {
+  
+    public int countPrefixes(String[] words, String s) {
+        int count = 0;
+        
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+  
+}
