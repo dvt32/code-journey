b5acb09a792ca353786aa86181e65855b95192a0
Dimitar Trifonov <dtrifonov32@gmail.com>
2024-01-20 23:16:46 +0200

    Create Find the Maximum Achievable Number.kt

---
+++ Kotlin/LeetCode/Find the Maximum Achievable Number.kt
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number
+
+class Solution {
+  
+    fun differenceOfSums(n: Int, m: Int): Int {
+        var num1 = 0;
+        var num2 = 0;
+
+        for (i in 1..n) {
+            if (i % m != 0) {
+                num1 += i;
+            } else {
+                num2 += i;
+            }
+        }
+
+        return num1 - num2;
+    }
+    
+}
