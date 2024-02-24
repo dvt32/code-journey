907f74fee43f5ff9192fd6fb9bbc274cca800938
Dimitar Trifonov <dtrifonov32@gmail.com>
2024-02-24 15:22:38 +0200

    Create Number of Employees Who Met the Target.kt

---
+++ Kotlin/LeetCode/Number of Employees Who Met the Target.kt
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/number-of-employees-who-met-the-target
+
+class Solution {
+
+    fun numberOfEmployeesWhoMetTarget(hours: IntArray, target: Int): Int {
+        var count = 0;
+
+        for (h in hours) {
+            if (h >= target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
