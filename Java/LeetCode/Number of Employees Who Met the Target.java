58caa9133fc4367df0fc68ef1a853d934dfbc407
Dimitar Trifonov <dtrifonov32@gmail.com>
2024-02-24 15:20:08 +0200

    Create Number of Employees Who Met the Target.java

---
+++ Java/LeetCode/Number of Employees Who Met the Target.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/number-of-employees-who-met-the-target
+
+class Solution {
+
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+
+        for (int h : hours) {
+            if (h >= target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    
+}
