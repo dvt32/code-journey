11db6c6760271867f50274133d84f63001153996
Dimitar Trifonov <dtrifonov32@gmail.com>
2024-12-27 15:12:22 +0200

    Create Number of Different Integers in a String.java

---
+++ Java/LeetCode/Number of Different Integers in a String.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/number-of-different-integers-in-a-string/
+
+import java.math.BigInteger;
+
+class Solution {
+    
+    public int numDifferentIntegers(String word) {
+        String[] nums = word.split("[a-z]+");
+        Set<BigInteger> uniqueNums = new HashSet<>();
+        
+        for (String num : nums) {
+            if (!num.isEmpty()) {
+                uniqueNums.add(new BigInteger(num));
+            }
+        }
+
+        return uniqueNums.size();
+    }
+
+}
