6a99d390bad6e968eed08ec90392b2ae07da94b2
Dimitar Trifonov <dtrifonov32@gmail.com>
2024-12-27 15:23:09 +0200

    Create Number of Different Integers in a String.kt

---
+++ Kotlin/LeetCode/Number of Different Integers in a String.kt
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/number-of-different-integers-in-a-string/
+
+class Solution {
+
+    fun numDifferentIntegers(word: String): Int {
+        val nums = word.split("\\D+".toRegex())
+        val uniqueNums = mutableSetOf<BigInteger>()
+
+        nums.forEach {
+            if (!it.isEmpty()) {
+                uniqueNums.add(BigInteger(it))
+            }
+        }
+
+        return uniqueNums.size
+    }
+
+}
