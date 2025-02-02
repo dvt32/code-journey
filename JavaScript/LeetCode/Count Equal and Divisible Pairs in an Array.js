8dc7aa0634b96ee3d01aababa2bc5915d4e942b3
Dimitar Trifonov <dtrifonov32@gmail.com>
2025-02-02 14:25:14 +0200

    Create Count Equal and Divisible Pairs in an Array.js

---
+++ JavaScript/LeetCode/Count Equal and Divisible Pairs in an Array.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countPairs = function(nums, k) {
+    var pairCount = 0;
+
+    for (let i = 0; i < nums.length-1; ++i) {
+        for (let j = i+1; j < nums.length; ++j) {
+            if (nums[i] == nums[j] && (i*j % k == 0)) {
+                pairCount++;
+            }
+        }
+    }
+
+    return pairCount;
+};
