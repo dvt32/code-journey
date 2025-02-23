881ecdd67c70dba6d6dcc7bbb2fe9b7893fa91eb
Dimitar Trifonov <dtrifonov32@gmail.com>
2025-02-23 03:53:46 +0200

    Create Best Poker Hand.java

---
+++ Java/LeetCode/Best Poker Hand.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/best-poker-hand/
+
+class Solution {
+
+    public String bestHand(int[] ranks, char[] suits) {
+        Set<Character> uniqueSuits = new HashSet<>();
+
+        for (char suit : suits) {
+            uniqueSuits.add(suit);
+        }
+        if (uniqueSuits.size() == 1) {
+            return "Flush";
+        }
+
+        Map<Integer, Integer> rankCounts = new HashMap<>();
+        boolean foundThree = false, foundTwo = false;
+        
+        for (int rank : ranks) {
+            rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
+        }
+        for (Integer rankCount : rankCounts.values()) {
+            if (rankCount >= 3) {
+                return "Three of a Kind";
+            } else if (rankCount == 2) {
+                foundTwo = true;
+            }
+        }
+        if (foundTwo) {
+            return "Pair";
+        }
+
+        return "High Card";
+    }
+
+}
