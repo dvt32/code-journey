b67af4791f495b21f1d56ceae023fa5b7af7cebf
Dimitar Trifonov <dtrifonov32@gmail.com>
2025-06-07 02:54:53 +0300

    Update Reverse Words in a String.java

--- Java/LeetCode/Reverse Words in a String.java
+++ Java/LeetCode/Reverse Words in a String.java
@@ -3,6 +3,20 @@
 import java.util.StringJoiner;
 
 class Solution {
+
+    public String reverseWords(String s) {
+        String[] words = s.trim().split(" +");
+        StringJoiner output = new StringJoiner(" ");
+
+        for (int i = words.length-1; i >= 0; --i) {
+            output.add(words[i]);
+        }
+
+        return output.toString();
+    }
+
+    // Alternative solution
+    /*
     public String reverseWords(String s) {
         String[] words = s.split(" +");
         
@@ -16,4 +30,6 @@ class Solution {
 
         return output.toString();
     }
+    */
+    
 }
