c359fed2e3bce11afc16b2a937f381425162c9c2
Dimitar Trifonov <dtrifonov32@gmail.com>
2025-12-31 21:48:02 +0200

    Create Count Almost Equal Pairs I.kt

---
+++ Kotlin/LeetCode/Count Almost Equal Pairs I.kt
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/count-almost-equal-pairs-i
+
+class Solution {
+    fun countPairs(nums: IntArray): Int {
+        var count = 0;
+
+        for (i in 0 until nums.size) {
+            for (j in i+1 until nums.size) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                    continue;
+                }
+
+                val x = if (nums[i] > nums[j]) nums[i].toString() else nums[j].toString();
+                val y = if (nums[i] > nums[j]) nums[j].toString() else nums[i].toString();
+                
+                var matched = false;
+                for (m in 0 until x.length) {
+                    if (matched) break;
+                    for (n in m+1 until x.length) {
+                        if (matched) break;
+                        val xSwapped = StringBuilder(x);
+                        xSwapped[m] = x[n];
+                        xSwapped[n] = x[m];
+                        if (xSwapped.toString().toInt() == y.toInt()) {
+                            count++;
+                            matched = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
