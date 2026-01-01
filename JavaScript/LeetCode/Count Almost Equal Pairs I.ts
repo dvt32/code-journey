243bb05b94bb9a5e11294153d7f1ff36f4149697
Dimitar Trifonov <dtrifonov32@gmail.com>
2026-01-01 15:33:22 +0200

    Create Count Almost Equal Pairs I.ts

---
+++ JavaScript/LeetCode/Count Almost Equal Pairs I.ts
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/count-almost-equal-pairs-i
+
+function countPairs(nums: number[]): number {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; ++i) {
+        for (let j = i+1; j < nums.length; ++j) {
+            if (nums[i] == nums[j]) {
+                count++;
+                continue;
+            }
+
+            let x = (nums[i] > nums[j] ? nums[i].toString() : nums[j].toString());
+            let y = (nums[i] > nums[j] ? nums[j] : nums[i]);
+            
+            let matched = false;
+            for (let m = 0; m < x.length && !matched; ++m) {
+                for (let n = m+1; n < x.length && !matched; ++n) {
+                    let xSwappedArr = x.split('');
+                    [xSwappedArr[m], xSwappedArr[n]] = [x[n], x[m]];
+                    let xSwapped = parseInt(xSwappedArr.join(''));
+                    if (xSwapped == y) {
+                        count++;
+                        matched = true;
+                    }
+                }
+            }
+        }
+    }
+
+    return count;
+};
