fae6d8e9e5c439b8320b0bb8f003a8b932f3dc16
Dimitar Trifonov <dtrifonov32@gmail.com>
2026-01-04 02:09:20 +0200

    Create Buddy Strings.ts

---
+++ JavaScript/LeetCode/Buddy Strings.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/buddy-strings
+
+function buddyStrings(s: string, goal: string): boolean {
+    if (s.length !== goal.length) {
+        return false;
+    }
+    
+    if (s === goal) {
+        return new Set(s).size < s.length;
+    }
+    
+    let mismatchingCharactersIndices = [];
+    for (let i = 0; i < s.length; ++i) {
+        if (s[i] !== goal[i]) {
+            mismatchingCharactersIndices.push(i)
+        }
+    }
+    
+    return (
+        mismatchingCharactersIndices.length === 2 &&
+        s[mismatchingCharactersIndices[0]] === goal[mismatchingCharactersIndices[1]] &&
+        s[mismatchingCharactersIndices[1]] === goal[mismatchingCharactersIndices[0]]
+    )
+};
